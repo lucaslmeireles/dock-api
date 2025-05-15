550de546e7e716ee7baf2ed7419bfe47505dc2d6
lucaslmeireles <86924068+lucaslmeireles@users.noreply.github.com>
2025-05-15 10:34:09 +0000

    feat: adding dynamic port

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var url = $"http://0.0.0.0:{port}";
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,4 +23,4 @@ if (app.Environment.IsDevelopment())
 app.DockRoutes();
 app.TruckRoutes();
 app.CargoRoutes();
-app.Run();
+app.Run(url);
